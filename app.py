48bcdb43514cc205634259d61c1767d0359f51cb
mesatk <muhammetesat.kiraz@hotmail.com>
2024-06-07 11:03:05 +0300

    refactor app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session, send_file
-import matplotlib.pyplot as plt
-import io
-import base64
-from asyncfair import FairnessAnalyzer
-import pandas as pd
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import io
+# import base64
+# import pandas as pd
+# import seaborn as sns
 import asyncio
 from controller.controller import Controller
 
@@ -53,59 +52,17 @@ def result():
     random_state = session.get('random_state', 'None')
     privileged_groups = session.get('privileged_groups', 'None')
 
-    async def analyze_fairness():
-        controller = Controller()
-        await controller.third_part()
-        return await controller.first_part()
+    
+    controller = Controller()
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    ress = loop.run_until_complete(analyze_fairness())
-    imglist = []
-
-    for name, result in ress.items():
-        print(f"\n{name}")
-        print(f"Accuracy: {result['accuracy']}")
-        print(f"Balanced Accuracy: {result['balanced_accuracy']}")
-        print(f"AUC-ROC: {result['auc_roc']}")
-        print(f"Classification Report:\n {pd.DataFrame(result['report']).transpose()}")
-        print(f"Disparate Impact: {result['disparate_impact']}")
-        print(f"Statistical Parity Difference: {result['statistical_parity_difference']}")
-        print(f"Equal Opportunity Difference: {result['equal_opportunity_difference']}")
-        print(f"Average Odds Difference: {result['average_odds_difference']}")
-        print(f"Theil Index: {result['theil_index']}")
-
-        bimg = io.BytesIO()
-        plt.figure(figsize=(8, 6))
-        sns.heatmap(result['conf_matrix'], annot=True, fmt="d", cmap="Blues", xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
-        plt.title(f'Confusion Matrix - {name}')
-        plt.xlabel('Predicted label')
-        plt.ylabel('True label')
-        plt.savefig(bimg, format='png')
-        bimg.seek(0)
-        bplot_url = base64.b64encode(bimg.getvalue()).decode()
-        imglist.append(bplot_url)
-
-    print("-------------------------------------")
-
-    # for element in imglist:
-    #     print(element)
-
-    # Verilerle grafiği oluşturma
-    values = [0.123, 0.258, 0.879]
-    img = io.BytesIO()
-    plt.figure(figsize=(10, 5))
-    plt.plot(values, marker='o')
-    plt.title('Example Data Plot')
-    plt.xlabel('Index')
-    plt.ylabel('Values')
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
+    imglist = loop.run_until_complete(controller.first_part_results_tables())
 
 
 
-    return render_template('result.html', progress=100, dataset=selected_dataset, model=selected_model, test_size=test_size, random_state=random_state, privileged_groups=privileged_groups, plot_url=plot_url, imglist=imglist)
+
+    return render_template('result.html', progress=100, dataset=selected_dataset, model=selected_model, test_size=test_size, random_state=random_state, privileged_groups=privileged_groups, imglist=imglist)
 
 @app.route('/team')
 def team():
--- controller/controller.py
+++ controller/controller.py
@@ -1,11 +1,13 @@
 import asyncio
 from service.thirdpart import ThirdPart
-# controller.py içeriği
 import sys
 import os
+import io
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import base64
 
-# `/service` dizinini `sys.path` içerisine ekleyin
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../service')))
 
 from service.firstpart import FirstPart
 # from service.secondPart import SecondPart
@@ -29,7 +31,23 @@ class Controller:
         analyzer = ThirdPart(test_size, random_state, privileged_group)
         await analyzer.run_experiment()
         analyzer.display_results()
+
+    async def first_part_results_tables(self, test_size=0.3, random_state=82, privileged_group="telephone"):
+        result = await self.first_part(test_size, random_state, privileged_group)
+        imglist = []
+        for name, result in result.items():
+            bimg = io.BytesIO()
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(result['conf_matrix'], annot=True, fmt="d", cmap="Blues", xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+            plt.title(f'Confusion Matrix - {name}')
+            plt.xlabel('Predicted label')
+            plt.ylabel('True label')
+            plt.savefig(bimg, format='png')
+            bimg.seek(0)
+            bplot_url = base64.b64encode(bimg.getvalue()).decode()
+            imglist.append(bplot_url)
         
+        return imglist
 
 # aa = AsyncWorker()
 # asyncio.run(aa.process_data())
